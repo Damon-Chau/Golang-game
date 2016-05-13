ef2fa1166c43656f8b44a77713cf6a9e9a43746b
Damon Chau <chau413@gmail.com>
2016-05-12 22:58:13 -0400

    Added combat functions and structs

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
+	"sort"
 )
 
 // All types 
@@ -28,6 +30,24 @@ type Game struct {
 	Current Location string
 }
 
+type Character struct {
+	Name string
+	Health int
+	Evasion int
+	Alive bool
+	Speed int
+	Weap int
+	Npc bool
+}
+
+type Weapon struct {
+	minAtt int
+	maxAtt int
+	Name string
+}
+
+type Players []Character
+
 var locationMap = map[string]*Location{
 	// various locations of the game
 	"Bridge":      {"You are on the bridge of a spaceship sitting in the Captain's chair.", []string{"Ready Room", "Turbo Lift"}, []string{"alienAttack"}},
@@ -45,8 +65,20 @@ var evts = map[string]*Event{
 	"relaxing":        {Chance: 100, Description: "In the lounge you are so relaxed that your health improves.", Health: +10, Evt: ""},
 }
 
-func (e *Event) ProcessEvent() int {
+var ennemies = map[int]*Character{
+	1: {Name: "Klingon", Health: 50, Alive: true, Weap: 2},
+	2: {Name: "Romulan", Health: 55, Alive: true, Weap: 3},
+}
 
+var Weaps = map[int]*Weapon{
+	// 3 types of weapons
+	1: {Name: "Phaser", minAtt: 5, maxAtt: 15},
+	2: {Name: "Klingon Disruptor", minAtt: 1, maxAtt: 15},
+	3: {Name: "Romulan Disruptor", minAtt: 3, maxAtt: 12},
+}
+
+func (e *Event) ProcessEvent() int {
+	// event struct
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
@@ -68,6 +100,35 @@ func (e *Event) ProcessEvent() int {
 
 }
 
+func (p *Character) Equip(w int) {
+	// Equip weapon selected
+	p.Weap = w
+}
+
+func (p *Character) Attack() int {
+	// UAttack function
+	return Weaps[p.Weap].Fire()
+}
+
+func (w *Weapon) Fire() int {
+	// corresponds to combat with weapons
+	return w.minAtt + rand.Intn(w.maxAtt - w.minAtt)
+}
+
+// Players
+func (slice Players) Len() int {
+	return len(slice)
+}
+
+func (slice Players) Less(i, j int) bool {
+	return slice[i].Speed > slice[j].Speed
+}
+
+func (slice Players) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+// Game logic
 func (g *Game) Play() {
     CurrentLocation = locationMap["Bridge"]
 	fmt.Println(g.Welcome)
@@ -101,6 +162,104 @@ func (g *Game) Play() {
 		// Go to user specified location                                         
 		CurrentLocation = locationMap[CurrentLocation.Transitions[newLoc]]
 	}
+}
+
+//Combat functions
+func RunBattle(players Players) {
+    sort.Sort(players)
+    
+	round := 1
+	numAlive := players.Len()
+	playerAction := 0
+	for {
+	    for x := 0; x < players.Len(); x++ {
+    		players[x].Evasion = 0      // Reset evasion for all characters
+    	}
+		DisplayInfo("Combat round", round, "begins...")
+        for x := 0; x < players.Len(); x++ {
+            if players[x].Alive != true {
+                continue
+            }
+            playerAction = 0
+            if !players[x].Npc {
+                DisplayInfo("DO you want to")
+                DisplayInfo("\t1 - Run")
+                DisplayInfo("\t2 - Evade")
+                DisplayInfo("\t3 - Attack")
+                GetUserInput(&playerAction)
+            }
+            if playerAction == 2 {
+                players[x].Evasion = rand.Intn(15)
+                DisplayInfo("Evasion set to:", players[x].Evasion)
+            }
+            tgt := selectTarget(players, x)
+            if tgt != -1 {
+                DisplayInfo("player: ", x, "target: ", tgt)
+                attp1 := players[x].Attack()
+                players[tgt].Health = players[tgt].Health - attp1
+                if players[tgt].Health <= 0 {
+                    players[tgt].Alive = false
+                    numAlive--
+                }
+                DisplayInfo(players[x].Name+" attacks and does", attp1, "points of damage with his", Weaps[players[x].Weap].Name, "to the ennemy.")
+            }
+        }
+		if endBattle(players) || playerAction == 1 {
+			break
+		} else {
+			DisplayInfo(players)
+			round++
+		}
+	}
+}
+
+func DisplayInfof(format string, args ...interface{}) {
+	fmt.Fprintf(Out, format, args...)
+}
+
+func DisplayInfo(args ...interface{}) {
+	fmt.Fprintln(Out, args...)
+}
+
+func GetUserInput(i *int) {
+	fmt.Fscan(In, i)
+}
+
+func endBattle(players []Character) bool {
+	count := make([]int, 2)
+	count[0] = 0
+	count[1] = 0
+	for _, pla := range players {
+		if pla.Alive {
+			if pla.Npc == false {
+				count[0]++
+			} else {
+				count[1]++
+			}
+		}
+	}
+	if count[0] == 0 || count[1] == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func selectTarget(players []Character, x int) int {
+	y := x
+	for {
+		y = y + 1
+		if y >= len(players) {
+			y = 0
+		}
+		if (players[y].Npc != players[x].Npc) && players[y].Alive {
+			return y
+		}
+		if y == x {
+			return -1
+		}
+	}
+	return -1
 }
 
 func (g *Game) ProcessEvents(events []string) {
